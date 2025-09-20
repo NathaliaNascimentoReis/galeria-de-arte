58b846bf48e6debacb5b6991206b36532161e62b
nat <nathalia.n.reis@aluno.senai.br>
2025-09-20 09:13:21 -0300

    feat: first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+const serverPort = process.env.PORT || 3001
+
+app.get("/", (req, res) => {
+    res.send("Servidor Funcionando 🖌️")
+});
+
+app.listen(serverPort, () => {
+    console.log(`🖼️  Servidor rodando em http://localhodt:${serverPort}`);
+})
