97a01ffe9c0f478134d4a109b6a84db6e005665b
nat <nathalia.n.reis@aluno.senai.br>
2025-09-20 14:14:53 -0300

    feat: inicia CREATE

--- src/controllers/obrasControllers.js
+++ src/controllers/obrasControllers.js
@@ -13,17 +13,111 @@ const getObrasById = (req, res) => {
   const obra = obrasDeArte.find((o) => o.id === id);
 
   if (obra) {
-    res.status(200).json({
-      sucess: true,
+    return res.status(200).json({
+      success: true,
       message: `Id ${id} encontrado!`,
       obra: obra,
     });
   }
 
-  res.status(400).json({
-    sucess: false,
+  return res.status(400).json({
+    success: false,
     message: "Obra não encontrada",
   });
 };
 
-export { getAllObras, getObrasById };
+const createObra = (req, res) => {
+  const {
+    titulo,
+    artista,
+    ano,
+    estilo,
+    status,
+    destaque,
+    valor,
+    dimensoes,
+    urlImagem,
+  } = req.body;
+
+  if (
+    !titulo ||
+    !estilo ||
+    !status ||
+    !destaque ||
+    !valor ||
+    !dimensoes ||
+    !urlImagem
+  ) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "Os campos a seguir são obrigatórios: título, estilo, status, destaque, valor, dimensoes, url da imagem",
+    });
+  }
+
+  const statusFormatado = status.toLowerCase();
+
+  const statusLista = [
+    "em exposição",
+    "fora de exposição",
+    "vendido",
+    "disponível",
+  ];
+
+  if (!statusLista){
+    return res.status(400).json({
+      success: false,
+      message:
+        "O campo 'status' deve ser um dos seguintes valores: " +
+        statusLista.join(", ") +
+        "!",
+    });
+  }
+
+  const valorNumerico = Number(valor);
+
+  if (isNaN(valorNumerico) || valorNumerico <= 0) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "O campo 'valor' deve ser um número válido (diferente de 0) e positivo.",
+    });
+  }
+
+  const obraExistente = obrasDeArte.some(
+    (obra) => obra.titulo === titulo && obra.artista === artista
+  );
+
+  if (obraExistente) {
+    return res.status(409).json({
+      success: false,
+      message:
+        "Esta obra de arte já existe na galeria. Tente um título ou artista diferente.",
+    });
+  }
+
+  const verificaId =
+    obrasDeArte.length > 0 ? obrasDeArte[obrasDeArte.length - 1].id + 1 : 1;
+
+  const novaObra = {
+    id: verificaId,
+    titulo: titulo.toLowerCase(),
+    artista: artista.toLowerCase(),
+    ano,
+    estilo: estilo.toLowerCase(),
+    status: status.toLowerCase(),
+    destaque,
+    valor,
+    dimensoes,
+    urlImagem: urlImagem.toLowerCase(),
+  };
+
+  obrasDeArte.push(novaObra);
+  res.status(201).json({
+    success: true,
+    message: "Obra adicionada ao acervo com sucesso!",
+    obra: novaObra,
+  });
+};
+
+export { getAllObras, getObrasById, createObra };
--- src/routes/obrasRoutes.js
+++ src/routes/obrasRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getAllObras, getObrasById } from "../controllers/obrasControllers.js";
+import {
+  getAllObras,
+  getObrasById,
+  createObra,
+} from "../controllers/obrasControllers.js";
 
 const router = express.Router();
 
 router.get("/", getAllObras);
 router.get("/:id", getObrasById);
+router.post("/", createObra);
 
 export default router;
